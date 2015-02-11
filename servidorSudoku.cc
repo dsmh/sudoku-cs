2a65d3957b7c655b148e1074bc7e19ba5714c093
dsmh <steven.mejia@gmail.com>
2015-02-11 12:31:29 -0500

    Segundo commit

--- servidorSudoku.cc
+++ servidorSudoku.cc
@@ -1,4 +1,5 @@
-//  Hacer un Servidor que procese operaciones aritmeticas en el formato "OP;n1;n2"
+//  Hacer un Servidor para jugar sudoku "Fil;Col;id;val"
+//TO DO: Validar, Hashtable for id score. 
 
 #include <zmq.h>
 #include <stdio.h>
@@ -6,8 +7,9 @@
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <iostream>
 
-
+using namespace std;
 
 /** Check whether grid[i][j] is valid in the grid */
 bool isValid(int grid[] [9])
@@ -37,51 +39,94 @@ bool isValid(int grid[] [9])
 		for (int j = 0; j < 9; j++)
 			if ((grid[i][j] < 0) || (grid[i][j] > 9))
 				status = false;
-	
 	return status;
 }
 
 
 
+void inicializarSudoku(int matrix[9][9])
+{
+	int i,j;
+	for (i=0; i<9; i++)
+	{
+		for(j=0;j<9;j++)
+		{
+			matrix[i][j]=0;
+		}
+	}
+	matrix[0][2]=4;
+	matrix[0][3]=3;
+	matrix[0][8]=6;
+	matrix[1][0]=6;
+	matrix[1][3]=1;
+	matrix[1][5]=9;
+	matrix[2][0]=7;
+	matrix[2][7]=4;
+	matrix[2][8]=9;
+	matrix[3][2]=1;
+	matrix[3][4]=8;
+	matrix[3][5]=5;
+	matrix[3][6]=4;
+	matrix[3][7]=6;
+	matrix[4][2]=6;
+	matrix[4][3]=2;
+	matrix[4][4]=9;
+	matrix[4][5]=3;
+	matrix[4][6]=7;
+	matrix[5][1]=7;
+	matrix[5][2]=5;
+	matrix[5][3]=4;
+	matrix[5][4]=1;
+	matrix[5][6]=9;
+	matrix[6][0]=4;
+	matrix[6][1]=6;
+	matrix[6][8]=7;
+	matrix[7][3]=9;
+	matrix[7][5]=4;
+	matrix[7][8]=1;
+	matrix[8][0]=2;
+	matrix[8][5]=7;
+	matrix[8][6]=8;	
+}
 
 
 
 
-
-
-
-
-
-
+///matriz[fila][col]
+///
 
 
 int main (void)
 {
+	int matriz[9][9];
+	inicializarSudoku(matriz);
+	//cout << matriz[0][0] << endl;
+
+	
+	
     //  Socket to talk to clients
     void *context = zmq_ctx_new ();
     void *responder = zmq_socket (context, ZMQ_REP); //LA CONSTANTE ES PARA RESPONDER SOLICITUDES
     int rc = zmq_bind (responder, "tcp://*:5555"); //*-> INTERFAZ DE RED POR DEFECTO
     assert (rc == 0);
     
-	char n1[50] = "";
-	char n2[50]= "";
-	char op[1]="";
-	int num1=0;
-	int num2=0;
-	int R=0;
-	float re=0.0;
-	char resultado[100]="";	
+	char id[50] = "";
+	int fila=0;
+	int col=0;
+	int valor;
+	int score=0;
     while (1) {
-        char buffer [101];
-        printf("True \n");
-        zmq_recv (responder, buffer, 101, 0);
+        char buffer [100];
+        printf(".............:::::::::::::::SUSOKU ONLINE:::::::::::::::............. \n");
+        zmq_recv (responder, buffer, 100, 0);
         printf ("%s\n",buffer);
-        /////////////
-        op[0]=buffer[0];
-        printf ("%s\n",op);
+      
+        ///printf ("%s\n",op);
      
-        ////OPERACION SUMA
-		sscanf(buffer,"%c;%d;%d", &op,&num1,&num2);
+        ////JUGADA
+		sscanf(buffer,"%d;%d;%s;%d", &fila,&col,&id);
+		cout << buffer << endl;
+		/*
 		if(op[0] == '+')
 		{
 			R=num1+num2;
@@ -113,6 +158,9 @@ int main (void)
         //sleep (5);          //  Do some 'work'
         
         zmq_send (responder, resultado, 100, 0);
+        * 
+        * */
     }
+        
     return 0;
 }
